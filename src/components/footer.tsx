c1f813a012d631e140662493c160de062309d277
demarc <demarcjohnson@gmail.com>
2020-11-25 23:52:03 -0500

    first commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export const Footer: React.FC = () => (
+  <footer>
+    <p>DeMarc Johnson &copy; 2020</p>
+  </footer>
+);
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export const Header: React.FC = () => (
+  <header>
+    <p>Where in the world?</p>
+  </header>
+);
